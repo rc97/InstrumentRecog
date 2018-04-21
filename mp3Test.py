584d50b9c905984b384ac4a0f90ff5d00f37dd3e
Rupesh Chinta <rupesh.ch97@gmail.com>
2018-04-21 15:30:06 -0400

    made plotting better

--- mp3Test.py
+++ mp3Test.py
@@ -15,14 +15,14 @@ vRawData = vSound.raw_data
 
 vSamples = np.fromstring(vRawData, dtype=np.int16)
 print('time', len(vSamples))
-plt.subplot(221)
-plt.plot(list(range(len(vSamples))), vSamples)
+plt.subplot(321)
+plt.plot(vSamples)
 # plt.show()
 
 vfft = fft.rfft(vSamples, n=N_FFT)[:8192]
 print('frequency', len(vfft))
-plt.subplot(222)
-plt.plot(list(range(len(vfft))), np.absolute(vfft))
+plt.subplot(322)
+plt.plot(np.absolute(vfft))
 # plt.show()
 
 fSound = AudioSegment.from_mp3("f1.mp3")
@@ -34,12 +34,31 @@ fRawData = fSound.raw_data
 
 fSamples = np.fromstring(fRawData, dtype=np.int16)
 print('time', len(fSamples))
-plt.subplot(223)
-plt.plot(list(range(len(fSamples))), fSamples)
+plt.subplot(323)
+plt.plot(fSamples)
 # plt.show()
 
 ffft = fft.rfft(fSamples, n=N_FFT)[:8192]
 print('frequency', len(ffft))
-plt.subplot(224)
-plt.plot(list(range(len(ffft))), np.absolute(ffft))
+plt.subplot(324)
+plt.plot(np.absolute(ffft))
+# plt.show()
+
+tSound = AudioSegment.from_mp3("t1.mp3")
+
+# get raw audio data as a bytestring
+# check the following to make sure
+# but we are assuming 2 byte samples and 1 channel
+tRawData = tSound.raw_data
+
+tSamples = np.fromstring(tRawData, dtype=np.int16)
+print('time', len(tSamples))
+plt.subplot(325)
+plt.plot(tSamples)
+# plt.show()
+
+tfft = fft.rfft(tSamples, n=N_FFT)[:8192]
+print('frequency', len(tfft))
+plt.subplot(326)
+plt.plot(np.absolute(tfft))
 plt.show()
