0282fdf1cf479fe0f016f17b42d5e13adf62ffc4
pgadani <pgadani123@gmail.com>
2018-04-22 02:47:43 -0400

    Visualization Take 2

--- soundTouchTest.py
+++ soundTouchTest.py
@@ -9,19 +9,19 @@ devices = discover_devices(timeout=10)
 for device in devices:
 	print(device.config.name + " - " + device.config.type)
 
-# device = soundtouch_device('192.168.1.72')
+device = soundtouch_device('192.168.1.72')
 
-# # Config object
-# print(device.config.name)
+# Config object
+print(device.config.name)
 
-# # Status object
-# # device.status() will do an HTTP request. Try to cache this value if needed.
-# status = device.status()
-# print(status.source)
-# print(status.artist + " - "+ status.track)
-# device.pause()
-# device.next_track()
-# device.play()
+# Status object
+# device.status() will do an HTTP request. Try to cache this value if needed.
+status = device.status()
+print(status.source)
+print(status.artist + " - "+ status.track)
+device.pause()
+device.next_track()
+device.play()
 
-# account_id = device.status().content_item.source_account
-# print(account_id)
+account_id = device.status().content_item.source_account
+print(account_id)
